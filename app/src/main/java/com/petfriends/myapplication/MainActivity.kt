d946aa94b16844e493940cb90d49a30224421c11
Tim <bso11246@gmail.com>
2021-11-15 15:11:57 +0900

    테스트커밋 (#5)
    Co-authored-by: DESKTOP-I2OGSEQ\manta <manta11246>

--- app/src/main/java/com/petfriends/myapplication/MainActivity.kt
+++ app/src/main/java/com/petfriends/myapplication/MainActivity.kt
@@ -18,6 +18,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+
         val moongchiPicker = createMoongchiPicker(
             mediaType = PetMediaType.IMAGE,
             allowPermissionRequest = true,
